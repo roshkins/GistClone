8e4d1d33b156309749db27d40ea8941a937c9f99
= <student@appacademy.io>
2013-08-29 11:25:53 -0700

    Initial commit.

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+SessionsTemplate::Application.routes.draw do
+  resources :users, :only => [:new, :create, :show]
+  resource :session, :only => [:new, :create, :destroy]
+
+  root :to => "users#new"
+end
